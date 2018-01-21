033ad9531b25327ee2a92d930384d19cba666281
JonasZHGIT <zhongjonas@gmail.com>
2018-01-21 23:53:24 +0800

    achieved short_url

--- practice3.js
+++ practice3.js
@@ -3,11 +3,13 @@ var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var axios = require('axios');
-var url_obj = {};
 var url_arr = [];
 
 http.createServer(function(req, res) {
-	if(req.url === '/') {
+	var status = false;
+	if(req.url === '/favicon.ico') {
+		res.end();
+	} else if(req.url === '/') {
 		var realPath = path.join(__dirname, '/front/index.html');
 		res.writeHeader(200, {"Content-Type": "text/html"});
 		fs.readFile(realPath, function(err, data) {
@@ -15,30 +17,47 @@ http.createServer(function(req, res) {
 			res.write(data.toString());
 			res.end();
 		});
-	} else if(req.url != '/favicon.ico') {
+	} else {
+		console.log(url_arr);
 		var pathname = url.parse(req.url).pathname.substring(1);
 		if(pathname.search(/new/i) === 0) {
 			pathname = pathname.substring(4);
 		}
-		axios.get(pathname).then(resp => {
-			console.log("yes");
-			var result = {
-				original_url: pathname,
-				short_url: url.parse(req.url).hostname + new Date().getTime()
-			};
-			url_obj.fake = result.short_url;
-			url_obj.real = pathname;
-			url_arr.push(url_obj);
-			console.log(url_obj);
-			res.end(JSON.stringify(result));
-		}).catch(err => {
-			console.log("no");
-			var error = {
-				error: 'URL invalid'
-			};
-			res.end(JSON.stringify(error));
+		url_arr.forEach(function(value) {
+			if(req.url === '/' + value.fake) {
+				res.writeHeader(302, {'Location': value.real});
+				res.end();
+			} else if(pathname === value.real) {
+				console.log("here");
+				status = true;
+				var result = {
+					original_url: pathname,
+					short_url: req.headers.host + '/' + value.fake
+				};
+				res.end(JSON.stringify(result));
+			}
 		});
-		
+		if(!status) {
+			axios.get(pathname).then(resp => {
+				console.log("yes");
+				var stamp = new Date().getTime();
+				var result = {
+					original_url: pathname,
+					short_url: req.headers.host + '/' + stamp
+				};
+				var url_obj = {};
+				url_obj.fake = stamp;
+				url_obj.real = pathname;
+				url_arr.push(url_obj);
+				console.log(url_obj);
+				res.end(JSON.stringify(result));
+			}).catch(err => {
+				console.log("no");
+				var error = {
+					error: 'URL invalid'
+				};
+				res.end(JSON.stringify(error));
+			});
+		}
 	}
-	
 }).listen(process.env.PORT || 8000);
